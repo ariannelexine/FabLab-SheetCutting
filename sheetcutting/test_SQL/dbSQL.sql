4476b9178898b2a7b0b6263097eda2afdb3fc6b3
Arianne Silvestre <ariannesilvestre23@yahoo.com>
2018-04-08 18:52:49 -0500

    updated sql

--- sheetcutting/test_SQL/dbSQL.sql
+++ sheetcutting/test_SQL/dbSQL.sql
@@ -40,3 +40,25 @@ WHERE variant_id = '0025' AND cut_id = 1);
 UPDATE sheet_inventory 
 SET staff_id = '000', removed_date = CURRENT_TIMESTAMP
 WHERE obj_id = 2; 
+
+-- Get parent of cut_size 10 x 10 
+SELECT cut_id FROM cut_sizes WHERE width = 10 AND height = 10; -- returns 6
+
+SELECT * FROM cut_sizes WHERE cut_id IN
+(SELECT parent_id FROM cutsize_children WHERE child_id = 6);
+
+use `fabapp-v0.9`;
+SELECT DISTINCT type, v.variant_id, name, width, height, price, count(obj_id) as 'In Stock'
+FROM sheet_type s
+LEFT JOIN cut_sizes as c ON s.type_id = c.type_id
+LEFT JOIN variants as v ON s.type_id = v.type_id
+LEFT JOIN sheet_inventory as i ON i.variant_id = v.variant_id AND i.cut_id = c.cut_id
+WHERE i.removed_date IS NULL AND v.variant_id = '1128'
+GROUP BY name, width, height;
+
+
+/* type
+variant
+count
+from parent */
+
