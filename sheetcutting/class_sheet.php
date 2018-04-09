395d6b988956f11a2f277a2b31039861eee2a880
DavidSM64 <benepe96@gmail.com>
2018-04-09 05:45:28 -0500

    Forgot to update class_sheet

--- sheetcutting/class_sheet.php
+++ sheetcutting/class_sheet.php
@@ -6,29 +6,31 @@ class Sheet
 	
 	public $material_name = "", $variant_name = "";
 	public $id = 0;
+	public $cut_id = 0;
     public $amount = 0;
 	public $width = 0, $height = 0;
 	public $price = 0;
 	
 	// Sheet class constructor.
-	function __construct($material, $variant, $id_num, $quantity, $w, $h, $cost) {
+	function __construct($material, $variant, $id_num, $cutId, $quantity, $w, $h, $cost) {
         $this->material_name = $material;
 		$this->variant_name = $variant;
 		$this->id = $id_num;
+		$this->cut_id = $cutId;
 		$this->amount = $quantity;
 		$this->width = $w;
 		$this->height = $h;
 		$this->price = $cost;
     }
 	
-	//
     public function toString() {
-		$display = $material_name . " ";
-		if($variant_name != NULL && $variant_name !== '') {
-			$display .= '(' . $variant_name . ') ';
+		$display = $this->material_name . " ";
+		if($this->variant_name != NULL && $this->variant_name !== '') {
+			$display .= '(' . $this->variant_name . ') ';
+			$display .= 'variant_id = ' . $this->id . ', cut_id = ' . $this->cut_id;
 		}
-		$display .= 'size = ' . $width . 'x' . $height . ', price = $' . $price;
-		return $display;
+		$display .= 'size = ' . $this->width . 'x' . $this->height . ', price = $' . $this->price;
+		return $display . ';';
     }
 }
 
